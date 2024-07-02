2d95e277848df2885e5af7da512d2706a4e63e0f
yjl <yjl_1567@qq.com>
2024-07-02 19:01:36 +0800

    feat: 抛出异常但不退出

--- include/pre.h
+++ include/pre.h
@@ -4,13 +4,17 @@
 #include <cstdio>
 #include <string>
 #include <windows.h>
+#include <stdexcept>
+#include <exception>
+
+#include "str-utils.h"
 
 inline void error(WCHAR *msg) {
     MessageBoxExW(NULL, msg, L"Error", MB_OK | MB_ICONERROR, 0);
-    ExitProcess(1);
+    throw std::runtime_error(wstring2string(msg));
 }
 
 inline void error(const WCHAR *msg) {
     MessageBoxExW(NULL, msg, L"Error", MB_OK | MB_ICONERROR, 0);
-    ExitProcess(1);
+    throw std::runtime_error(wstring2string(msg));
 }
